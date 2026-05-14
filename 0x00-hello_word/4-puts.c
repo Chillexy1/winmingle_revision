117e7fb2f5c37a83760e258f4bd9c14f64614e28
chillexy <chillexystev96@gmail.com>
2026-05-14 16:34:53 +0100

    this a c file printed to the terminal using puts instead

---
+++ 0x00-hello_word/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/*
+*Author: Chillexy Steven                                                           
+*Program: WinMingle Community C Training
+*Description: this program peints out a statement to the terminal using puts instead of printf
+*/
+
+int main(void)
+{
+    puts("\"programming is like building a multilingual puzzle");
+    
+    return 0;
+}
