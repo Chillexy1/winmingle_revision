a400576d6bb8e4a7bdb110b885fede18ff75d7da
chillexy <chillexystev96@gmail.com>
2026-05-25 19:48:50 +0100

    this a function that returns the sum of two integers number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/*
+ *Author: Chillexy Steven
+ *Program: WinMingle Community C Training
+ *Description: this a function that adds two integer numbers and returns its
+ * result
+ */
+
+int add(int a, int b) { return a + b; }
