4d04da3167dfadbcbc0bf55ef683338fa2f32a50
chillexy <chillexystev96@gmail.com>
2026-06-03 17:27:33 +0100

    adds a function that takes a pointer to an int as a parameter

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/*
+ *Author: Chillexy Steven
+ *Program: WinMingle Community C Training
+ *Description: this is a function that takes a pointer to an int as a parameter
+ */
+
+void reset_to_98(int *n) { *n = 98; }
