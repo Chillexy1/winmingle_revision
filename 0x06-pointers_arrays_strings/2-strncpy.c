ccb0c3bd97db16f92153823cb7291272aee002a3
chillexy <chillexystev96@gmail.com>
2026-06-09 07:49:36 +0100

    adds a function that copies a golr strings using n bytes of src

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ *Author: Chillexy Steven
+ *Program: WinMingle Community C Training
+ *Description: copies a string from src to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n) {
+  int i = 0;
+
+  while (src[i] && i < n) {
+    dest[i] = src[i];
+
+    i++;
+  }
+
+  while (i < n) {
+    dest[i] = '\0';
+    i++;
+  }
+
+  return dest;
+}
