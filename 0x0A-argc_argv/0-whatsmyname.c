27d1f1775c7d214ce3584d23bddca981c1a47ab4
chillexy <chillexystev96@gmail.com>
2026-06-20 21:30:04 +0100

    adds a programe that prints out it's own name to the terminal

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ *Author: Chillexy Steven
+ *Program: WinMingle Community C Training
+ *Description: this a program that prints out its own name.
+ **/
+
+int main(int argc, char *argv[]) {
+  argc = 0;
+
+  printf("%s\n", argv[argc]);
+  return 0;
+}
