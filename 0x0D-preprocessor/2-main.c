d1768d6c9dcc34a1ae21ccf1c5ed93d5abcef864
chillexy <chillexystev96@gmail.com>
2026-07-08 21:36:19 +0100

    adds a program that prints out the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ *Author: Chillexy Steven
+ *Program: WinMingle Community C Training
+ *Description: writes a program that prints the name of the file it was compiled
+ * from follwed by a new line.
+ */
+
+/*
+ * N.B: WE'RE ALLOWED TO USE THE STANDARD LIBRARY IN THIS EXERCISE
+ * */
+
+int main(void) {
+  printf("%s\n", __FILE__);
+  return (0);
+}
